006581bab7a7815a9c72619994c40c537dcaf4ee
Ahmed Maqbool <ahmedmaqx11@gmail.com>
2021-10-18 19:08:00 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+const app = express();
+import fetch from "node-fetch";
+import cors from "cors";
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello To The API");
+});
+
+app.get("/repo", async (req, res) => {
+  const response = await fetch();
+  const data = await response.json(process.env.REPO_API);
+
+  res.json(data);
+});
+
+app.get("/developers", async (req, res) => {
+  const response = await fetch(process.env.DEV_API);
+
+  const data = await response.json();
+
+  res.json(data);
+});
+
+app.listen(1000 || process.env.PORT, () => {
+  console.log("server started");
+});
